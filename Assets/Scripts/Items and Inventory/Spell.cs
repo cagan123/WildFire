e9683461c2b450bb3cf0d591ee6a4f2e73aa4707
cagan123 <ca2003gan@gmail.com>
2025-01-29 00:18:21 +0300

    spell changes

--- Assets/Scripts/Items and Inventory/Spell.cs
+++ Assets/Scripts/Items and Inventory/Spell.cs
@@ -6,8 +6,10 @@ public class Spell : ScriptableObject
     public string spellName; // Name of the spell
     public string description; // Description of the spell
     public int damage; // Damage of the spell
+    public int poise; // Poise of the spell
     public string statusEffects; // Status effects of the spell
     public Sprite Icon; // Icon of the spell
+    public GameObject spellPrefab; // Prefab of the spell
 
     public Spell(string name, Sprite icon)
     {
